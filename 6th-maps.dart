4541515bc915d349cfce69c38b0fd999924207b1
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-11-22 20:04:58 +0530

    MAPS

--- 6th-maps.dart
+++ 6th-maps.dart
@@ -1,3 +1,16 @@
-void main(){
-  
+void main() {
+  //   var planet = {"first": "mercury",1:"rabdon"};
+  Map<int, String> planets = {
+    1: "mercury",
+    2: "Venus",
+    3: "Earth",
+    4: "Mars",
+    5: "Jupiter",
+  };
+  planets[1] = "Random";
+  planets[6] = "Uranus";
+  print(planets);
+  print(planets[1]);
+//   it will tell that key is there in the map or not
+  print(planets.containsKey(1));
 }
