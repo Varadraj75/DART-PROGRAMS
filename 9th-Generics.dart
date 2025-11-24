e1d0cb80485f46515d8db3673532349412e85445
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-11-24 10:34:59 +0530

    Generics

---
+++ 9th-Generics.dart
@@ -0,0 +1,57 @@
+void main() {
+  var noodles = MenuItem('Veg Noodles', 9.99);
+  var pizza = MenuItem('Volcano Pizza', 12.99);
+  var pizzass = Pizza(["Mushrooms", "Panner"], "Veg Volcano", 15.99);
+  print(pizzass.title);
+  print(noodles.title);
+  print(pizza.title);
+  var foods = Collection<MenuItem>('Menu Items', ["noodles", "pizza", "roast", "kebab"]);
+  var random = foods.randomItem();
+  print(random);
+}
+
+class MenuItem {
+  String title = "pizza";
+  double price = 9.99;
+  MenuItem(this.title, this.price);
+  String format() {
+    return "$title --> $price";
+  }
+
+  @override
+  String toString() {
+    return format();
+  }
+}
+
+class Pizza extends MenuItem {
+  List<String> toppings;
+
+  // Pizza(this.toppings, String title, double price) : super(title, price);
+  Pizza(this.toppings, super.title, super.price);
+  @override
+  String format() {
+    var formattedToppings = 'Contains: ';
+    for (final t in toppings) {
+      formattedToppings = '$formattedToppings $t';
+    }
+    return '$title -> $price \n$formattedToppings';
+  }
+
+  @override
+  String toString() {
+    return 'Instance of pizza: $title, $price, $toppings';
+  }
+}
+
+class Collection<T>{
+  String name;
+  List<T> data;
+
+  Collection(this.name, this.data);
+
+  T randomItem() {
+    data.shuffle();
+    return data[0];
+  }
+}
